313b97a41cea207473369ce76e5091b158d7bce1
Jordan <mihlali.jordan@standardbank.co.za>
2024-02-09 11:16:01 +0200

    adjust dimensions of floating file tree

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -8,7 +8,7 @@ return {
       window = {
         position = "float",
         popup = { -- settings that apply to float position only
-          size = { height = "17", width = "45" },
+          size = { height = "17", width = "65" },
           position = "50%", -- 50% means center it
         },
       },
