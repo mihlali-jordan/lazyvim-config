e6e6fd20b8c06561f18817e8b5629897e5e0c5c3
mihlali-jordan <biz@jordanmihlali.dev>
2024-06-05 08:59:40 +0200

    let lazy manage extra plugin

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -14,4 +14,13 @@ return {
       },
     },
   },
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    event = "VeryLazy",
+    opts = function(_, opts)
+      local icon = require("lazyvim.config").icons.kinds.TabNine
+      table.insert(opts.sections.lualine_x, 2, LazyVim.lualine.cmp_source("cmp_tabnine", icon))
+    end,
+  },
 }
